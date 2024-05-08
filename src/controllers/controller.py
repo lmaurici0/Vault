15f5fc799856d1516bff274f02e9ba395f00be50
eric <ericluismauricio@gmail.com>
2024-05-08 14:51:28 -0300

    fix: fix update route

--- src/controllers/controller.py
+++ src/controllers/controller.py
@@ -9,9 +9,12 @@ class HelloController(MethodView):
     def post(self):
         pass
 
+class LoginController(MethodView):
+    def post():
+        return render_template('public/login.html')
 
 class IndexController(MethodView):
-    def get(self,code=None):
+    def get(self):
         with mysql.cursor() as cur:
             cur.execute("SELECT * FROM produto")
             data = cur.fetchall()
@@ -19,20 +22,13 @@ class IndexController(MethodView):
             cur.execute("SELECT * FROM category")
             categories = cur.fetchall()
             
-            
-            if code is not None:
-                cur.execute("SELECT * FROM produto WHERE code = %s", (code,))
-                product = cur.fetchone()
-            else:
-                product = None
-        return render_template('public/index.html', data=data, product=product, categories=categories)
+        return render_template('public/index.html', data=data, categories=categories)
     
     def post(self):
         code = request.form['code']
         name = request.form['name']
         stock = request.form['stock']
         value = request.form['value']
-        #category = request.form['category']
         
         with mysql.cursor() as cur:
             cur.execute("INSERT INTO produto (code, name, stock, value) VALUES (%s, %s, %s, %s)", (code, name, stock, value))
