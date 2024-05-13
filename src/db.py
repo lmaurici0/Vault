724615e8fa01e85bd65a0512afa66d175b3d2ce7
Eric Luis <135241097+lmaurici0@users.noreply.github.com>
2024-05-12 21:01:08 -0300

    refactor: change user in db

--- src/db.py
+++ src/db.py
@@ -1,3 +1,3 @@
 import pymysql
 
-mysql = pymysql.connect(host='localhost', port=3306, user='root', password='cenapasslogin', database='vault_database')
+mysql = pymysql.connect(host='localhost', port=3306, user='root', password='your_pass', database='your_db')
